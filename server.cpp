8bcb7debc09397b854158d6979ee7fc6aeb755f7
fabian <famercado24@gmail.com>
2024-05-13 20:08:33 -0400

    clinete de react

--- server.cpp
+++ server.cpp
@@ -24,7 +24,7 @@ Server::Server(int port) : list_client(), mtx_client(), mtx_fds() //, list_room(
 
     // memset(&serv_addr, 0, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
     serv_addr.sin_port = htons(port);
 
     if (bind(lis_sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
